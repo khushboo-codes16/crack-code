47a81754524f64e5196df504ec7dfb98d5789628
Khushboo Kushwaha <113235556+khushboo-codes16@users.noreply.github.com>
2025-01-23 23:19:15 +0530

    Create running-sum_1DArray.py
    DAY 7 LeetCode

---
+++ running-sum_1DArray.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]  # Update each element to be the sum of itself and the previous elements
+        return nums
