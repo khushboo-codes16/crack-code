bd437d723018dab6246dba0da51ecdf7b6894b59
Khushboo Kushwaha <113235556+khushboo-codes16@users.noreply.github.com>
2025-02-22 23:18:53 +0530

    Create Intersection of Two Arrays.py
    DAY 31 LeetCode

---
+++ Intersection of Two Arrays.py
@@ -0,0 +1,60 @@
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        depth = 0
+        
+        for log in logs:
+            if log == "../":
+                if depth > 0:
+                    depth -= 1
+            elif log == "./":
+                continue
+            else:
+                depth += 1
+        
+        return depth
+    
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+        
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return dummy.next
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+        
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        
+        return prev
+    
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return head
+    
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
