74b6403e13cdc1b2b42480f9f2aaf794b0898636
Khushboo Kushwaha <113235556+khushboo-codes16@users.noreply.github.com>
2025-02-25 17:52:20 +0530

    Create Intersection of Two Arrays II.py
    DAY 34 LeetCode

---
+++ Intersection of Two Arrays II.py
@@ -0,0 +1,86 @@
+from typing import List, Optional
+from collections import Counter
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        depth = 0
+        
+        for log in logs:
+            if log == "../":
+                depth = max(0, depth - 1)
+            elif log != "./":
+                depth += 1
+        
+        return depth
+    
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        current = dummy
+        
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return dummy.next
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, current = None, head
+        
+        while current:
+            current.next, prev, current = prev, current, current.next
+        
+        return prev
+    
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        
+        return head
+    
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
+    
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        return [list(set(nums1) - set(nums2)), list(set(nums2) - set(nums1))]
+    
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return [num for num in (Counter(nums1) & Counter(nums2)).elements()]
+
+class MyHashMap:
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+    
+    def _hash(self, key: int) -> int:
+        return key % self.size
+    
+    def put(self, key: int, value: int) -> None:
+        index = self._hash(key)
+        for pair in self.buckets[index]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+        self.buckets[index].append([key, value])
+    
+    def get(self, key: int) -> int:
+        index = self._hash(key)
+        for pair in self.buckets[index]:
+            if pair[0] == key:
+                return pair[1]
+        return -1
+    
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        self.buckets[index] = [pair for pair in self.buckets[index] if pair[0] != key]
